92455f603c8f4f1849bde74296f6a5c7240eeccd
sorazo <picanarachu@outlook.com>
2025-06-15 18:36:21 +0900

    bootstrapを使用するための変更とtailwindの削除。

--- AtlasSNS_ver9/vite.config.js
+++ AtlasSNS_ver9/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+        host: 'localhost',
+        protocol: 'ws',
+        },
+    },
     plugins: [
         laravel({
             input: [
